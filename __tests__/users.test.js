64237ae2841f1b343c52127829bed4779badbbb3
Dave Fisher <dave.j.fish@gmail.com>
2022-08-23 10:29:45 -0700

    tests written for sign up login route

---
+++ __tests__/users.test.js
@@ -0,0 +1,41 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const testUser = {
+  email: 'test@exmaple.com',
+  password: '123456'
+};
+
+const registerAndSignIn = async (props = {}) => {
+  const mockUser = {
+    ...testUser,
+    ...props
+  };
+  
+  const agent = request.agent(app);
+
+  const response = await agent
+    .post('/api/v1/user/sessions')
+    .send(mockUser);
+  
+  const user = response.body;
+  
+  return [agent, user];
+};
+
+describe('tests for user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  it('#POST api/v1/user/sessions should create and sign in a new user', async () => {
+    const response = await registerAndSignIn();
+    expect(response.status).toBe(200);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+});
