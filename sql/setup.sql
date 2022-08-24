0ecd57627bdf2192bb93a5d6b98b62fd1ba389b4
Dave Fisher <dave.j.fish@gmail.com>
2022-08-23 18:30:45 -0700

    fixed problem with double login

--- sql/setup.sql
+++ sql/setup.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS userbase;
 CREATE TABLE userbase (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email TEXT NOT NULL,
-  passwordHash TEXT NOT NULL
+  password_hash TEXT NOT NULL
 );
 
 CREATE TABLE todos (
